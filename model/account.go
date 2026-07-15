f481be778a4993d4cbf11b2c69d25cf17c3ceda8
binbin0227 <1210850015@qq.com>
2026-07-15 13:37:14 +0800

    fix:

--- model/account.go
+++ model/account.go
@@ -6,5 +6,6 @@ type Account struct {
 	ID        int64     `gorm:"primaryKey;autoIncrement:false" json:"id"` 
 	Username  string    `gorm:"type:varchar(32);uniqueIndex;not null" json:"username"`
 	Password  string    `gorm:"type:varchar(255);not null" json:"-"`
+	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
