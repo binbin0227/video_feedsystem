8834d5d52f9ee867bba4ed248cac118b89494b5b
binbin0227 <1210850015@qq.com>
2026-07-15 13:34:42 +0800

    fix:补上漏写的字段

---
+++ dal/db/account.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"context"
+	"video_feedsystem/model"
+)
+
+// 将新用户信息写入数据库
+func CreateAccount(ctx context.Context, account *model.Account) error {
+	err := DB.WithContext(ctx).Create(*account).Error
+	return err
+}
+
+// 检查用户名是否已经被注册
+func CheckUsernameExist(ctx context.Context, username string) (bool, error) {
+	var count int64
+	err := DB.WithContext(ctx).Model(&model.Account{}).Where("username = ?", username).Count(&count).Error
+	if err != nil {
+		return false, err
+	}
+	return count > 0, nil
+}
--- model/video.go
+++ model/video.go
@@ -10,6 +10,7 @@ type Video struct {
 	PlayURL     string    `gorm:"type:varchar(255);not null" json:"play_url"`
 	CoverURL    string    `gorm:"type:varchar(255);not null" json:"cover_url"`
 	CreatedAt   time.Time `gorm:"index" json:"created_at"` // 刷视频时需要按时间倒序排,因此加上索引
+	UpdatedAt   time.Time `json:"updated_at"`
 	LikeCount   int       `gorm:"default:0" json:"like_count"`
 	Popularity  int       `gorm:"default:0" json:"popularity"`
 }
